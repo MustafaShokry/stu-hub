2bf8cf142bce0e3cd414f8a311d876d7313f7db6
Mostafa Shokry El-Kamel <mostafashokry2222@gmail.com>
2024-06-23 01:53:16 +0300

    .

--- src/app/(auth)/signUp/page.jsx
+++ src/app/(auth)/signUp/page.jsx
@@ -1,124 +1,4 @@
 'use client';
-// import { Form, Input, Button, Checkbox, Select } from 'antd';
-
-// const { Option } = Select;
-
-// const SignUpForm = () => {
-//   const onFinish = (values) => {
-//     console.log('Received values of form: ', values);
-//   };
-
-//   return (
-//     <Form
-//       name="signup"
-//       onFinish={onFinish}
-//       scrollToFirstError
-//       style={{ maxWidth: '400px', margin: 'auto', padding: '20px', background: '#fff', borderRadius: '8px' }}
-//     >
-//       <h2>Sign Up</h2>
-//       <Form.Item
-//         name="username"
-//         label="Username"
-//         rules={[
-//           {
-//             required: true,
-//             message: 'Please input your Username!',
-//           },
-//         ]}
-//       >
-//         <Input />
-//       </Form.Item>
-
-//       <Form.Item
-//         name="email"
-//         label="Email"
-//         rules={[
-//           {
-//             type: 'email',
-//             message: 'The input is not valid E-mail!',
-//           },
-//           {
-//             required: true,
-//             message: 'Please input your E-mail!',
-//           },
-//         ]}
-//       >
-//         <Input />
-//       </Form.Item>
-
-//       <Form.Item
-//         name="password"
-//         label="Password"
-//         rules={[
-//           {
-//             required: true,
-//             message: 'Please input your password!',
-//           },
-//         ]}
-//         hasFeedback
-//       >
-//         <Input.Password />
-//       </Form.Item>
-
-//       <Form.Item
-//         name="confirm"
-//         label="Confirm Password"
-//         dependencies={['password']}
-//         hasFeedback
-//         rules={[
-//           {
-//             required: true,
-//             message: 'Please confirm your password!',
-//           },
-//           ({ getFieldValue }) => ({
-//             validator(_, value) {
-//               if (!value || getFieldValue('password') === value) {
-//                 return Promise.resolve();
-//               }
-//               return Promise.reject(new Error('The two passwords that you entered do not match!'));
-//             },
-//           }),
-//         ]}
-//       >
-//         <Input.Password />
-//       </Form.Item>
-
-//       <Form.Item
-//         name="role"
-//         label="Role"
-//         rules={[{ required: true, message: 'Please select your role!' }]}
-//       >
-//         <Select placeholder="Select your role">
-//           <Option value="user">User</Option>
-//           <Option value="instructor">Instructor</Option>
-//         </Select>
-//       </Form.Item>
-
-//       <Form.Item
-//         name="agreement"
-//         valuePropName="checked"
-//         rules={[
-//           {
-//             validator: (_, value) =>
-//               value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
-//           },
-//         ]}
-//       >
-//         <Checkbox>
-//           I have read the <a href="">agreement</a>
-//         </Checkbox>
-//       </Form.Item>
-
-//       <Form.Item>
-//         <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
-//           Sign Up
-//         </Button>
-//       </Form.Item>
-//     </Form>
-//   );
-// };
-
-// export default SignUpForm;
 
 import React, { useState } from "react";
 import "./SignUp.css";
@@ -175,7 +55,7 @@ const SignUp = () => {
       console.log("SignUp successful:", response.data);
       localStorage.setItem("token", response.data.token);
       localStorage.setItem("user", JSON.stringify(response.data.data));
-      router.push("/featured");
+      router.push("/courses");
     } catch (error) {
       console.error("There was a problem with the signup request:", error);
       if (error.response && error.response.data && error.response.data.errors) {
@@ -188,12 +68,6 @@ const SignUp = () => {
 
   return (
     <div className="signup">
-      {/* <div className="backWhite">
-        <div className="logo">
-          <i className="fa-solid fa-book-open"></i>
-          <h1>STU-HUB</h1>
-        </div>
-      </div> */}
       <div className="title">
         <h1>Sign Up</h1>
         <p>Join STU-HUB and start learning</p>
