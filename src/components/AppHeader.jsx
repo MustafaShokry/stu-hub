3da015304dbbdedbb6a56098ed6f894af52d7c8c
Mostafa Shokry El-Kamel <mostafashokry2222@gmail.com>
2024-06-23 04:30:26 +0300

    .

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -5,7 +5,7 @@ import { UserOutlined, DownOutlined, SettingOutlined, LogoutOutlined } from '@an
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
-import logo from '../../public/logo.png';
+import logo from '../../public/file.jpg';
 const { Header } = Layout;
 
 
